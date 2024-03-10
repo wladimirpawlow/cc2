1677630cd2e9378b2d47f4d2e2cf018dc1ff23b0
wladimirpawlow <8105032@mail.ru>
2024-03-10 23:03:23 +0300

    MD covered with exceptions

--- be-cc/main.py
+++ be-cc/main.py
@@ -1,13 +1,13 @@
-from mod.modBusTCP import mbRead
-from mod.modBusRTU import mbRTURead
-import serial.tools.list_ports
+# import serial.tools.list_ports
+from mb_work.mod_bus_tcp import mbTCPRead
+from mb_work.mod_bus_rtu import mbRTURead
 
 print('start EW')
 # ports = serial.tools.list_ports.comports()
 # for port, desc, hwid in sorted(ports):
 #     print("{}: {} [{}]".format(port, desc, hwid))
 
-
+mbTCPRead()
 mbRTURead()
 
 
--- be-cc/mb_work/mod_bus_tcp.py
+++ be-cc/mb_work/mod_bus_tcp.py
@@ -2,7 +2,7 @@ from pymodbus.client import ModbusTcpClient
 
 client = ModbusTcpClient('10.10.100.100', 8899)
 
-def mbRead():
+def mbTCPRead():
     try:
         client.connect()
         result = client.read_holding_registers(0, 5,  slave=1)
